128a52213dc52a6fbd33c1407e6358f255faf958
Shailendra0801 <110927376+Shailendra0801@users.noreply.github.com>
2024-09-20 23:29:30 +0530

    Time: 5 ms (99.88%), Space: 45.9 MB (61.15%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,22 +1,47 @@
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         for(int i = 0; i < matrix.length; i++){
+//             if(getAns(matrix[i], target))
+//                 return true;
+//         }
+//         return false;
+//     }
+//     private boolean getAns(int[] arr, int t){
+//         int l = 0;
+//         int r = arr.length-1;
+//         while(l <= r){
+//             int mid = l + (r-l)/2;
+//             if(arr[mid] == t)
+//                 return true;
+//             else if(arr[mid] < t)
+//                 l = mid+1;
+//             else
+//                 r = mid-1;
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i = 0; i < matrix.length; i++){
-            if(getAns(matrix[i], target))
+    public boolean searchMatrix(int[][] matrix, int target)
+     {
+        int row=0;
+        int col=matrix[0].length-1;
+
+        while(row<=matrix.length-1 && col>=0)
+        {
+            if(matrix[row][col]==target)
+            {
                 return true;
-        }
-        return false;
-    }
-    private boolean getAns(int[] arr, int t){
-        int l = 0;
-        int r = arr.length-1;
-        while(l <= r){
-            int mid = l + (r-l)/2;
-            if(arr[mid] == t)
-                return true;
-            else if(arr[mid] < t)
-                l = mid+1;
+            }
+            else if(target<matrix[row][col])
+            {
+                col--;
+            }
             else
-                r = mid-1;
+            {
+                row++;
+            }
         }
         return false;
     }
