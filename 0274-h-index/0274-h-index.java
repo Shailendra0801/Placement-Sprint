97d671bc1fa2b587a401bd66e879180bc4eb6670
Shailendra0801 <110927376+Shailendra0801@users.noreply.github.com>
2024-09-30 23:40:35 +0530

    Time: 2 ms (72.53%), Space: 41.3 MB (68.3%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hIndex(int[] arr) {
+        Arrays.sort(arr);
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] >= arr.length-i)
+                return arr.length-i;
+        }
+
+        return 0;
+
+    }
+}
