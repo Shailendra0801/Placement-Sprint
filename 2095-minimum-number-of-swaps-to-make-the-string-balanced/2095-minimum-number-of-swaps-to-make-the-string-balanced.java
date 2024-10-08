a9b55d99798fc517103f32a09e0c0b6b7d7e14f0
Shailendra0801 <110927376+Shailendra0801@users.noreply.github.com>
2024-10-08 10:15:08 +0530

    Time: 15 ms (65.33%), Space: 54.1 MB (70.84%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,34 +1,22 @@
 class Solution {
     public int minSwaps(String s) {
-        // int n = s.length();
-        // int bal = 0, ans = 0;
+        int n = s.length();
+        int bal = 0, ans = 0;
 
-        // for(int i = 0; i < n; i++){
-        //     if(s.charAt(i) == '[')
-        //         bal++;
-        //     else
-        //         bal--;
+        for(int i = 0; i < n; i++){
+            if(s.charAt(i) == '[')
+                bal++;
+            else
+                bal--;
             
-        //     if(bal < 0){
-        //         ans++;
-        //         bal = 1;
-        //     }
-        // }
-
-        // return ans;
-
-        Stack<Character> stack = new Stack<>();
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            if(ch == '[')
-                stack.push(ch);
-            else{
-                if(stack.isEmpty() || stack.peek() == ']')
-                    stack.push(ch);
-                else
-                    stack.pop();
+            if(bal < 0){
+                ans++;
+                bal = 1;
             }
         }
-        return ((stack.size()/2)+1)/2;
+
+        return ans;
+
+       
     }
 }
