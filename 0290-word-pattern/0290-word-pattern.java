989e0f3ce04466afd2ff631fa859801349275c73
Shailendra0801 <110927376+Shailendra0801@users.noreply.github.com>
2024-10-09 22:21:35 +0530

    Time: 1 ms (81.34%), Space: 41.2 MB (81.7%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -5,19 +5,21 @@ class Solution {
             return false;
         
         Map<Character, String> mp = new HashMap<>();
-        Map<String, Character> mps = new HashMap<>();
+        // Map<String, Character> mps = new HashMap<>();
         for(int i = 0; i < cmp.length; i++){
             if(mp.containsKey(pattern.charAt(i))){
                 if(!mp.get(pattern.charAt(i)).equals(cmp[i]))
                     return false;
             }
             else{
-                if(mps.containsKey(cmp[i]))
+                // if(mps.containsKey(cmp[i]))
+                //     return false;
+                if(mp.containsValue(cmp[i]))
                     return false;
             }
 
             mp.put(pattern.charAt(i), cmp[i]);
-            mps.put(cmp[i], pattern.charAt(i));
+            // mps.put(cmp[i], pattern.charAt(i));
         }
 
         return true;
