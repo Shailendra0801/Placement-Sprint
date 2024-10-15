e928fec2d42b82a87e5f9a9b317a12c36fdc0f6a
Shailendra0801 <110927376+Shailendra0801@users.noreply.github.com>
2024-10-15 10:45:10 +0530

    Time: 35 ms (5.02%), Space: 45.3 MB (54.12%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long minimumSteps(String s) {
+        long ans = 0;
+        StringBuffer str = new StringBuffer(s);
+        int i = 0, j = str.length()-1;
+        while(i <= j){
+            if(str.charAt(i) == '1' && str.charAt(j) == '0'){
+                char temp = str.charAt(i);
+                str.setCharAt(i, str.charAt(j));
+                str.setCharAt(j, temp);
+                ans += j-i;
+            }
+            else if(str.charAt(i) == '1' && str.charAt(j) != '0')
+                i--;
+            else if(str.charAt(i) != '1' && str.charAt(j) == '0')
+                j++;
+
+            
+            i++;
+            j--;
+        }
+        return ans;
+    }
+}
