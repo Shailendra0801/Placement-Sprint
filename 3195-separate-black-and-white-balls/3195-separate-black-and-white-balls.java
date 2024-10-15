430fbf3b9809f0db274cfedef1814b2c8a81cf47
Shailendra0801 <110927376+Shailendra0801@users.noreply.github.com>
2024-10-15 10:47:45 +0530

    Time: 7 ms (100%), Space: 45.2 MB (70.97%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -1,23 +1,30 @@
 class Solution {
     public long minimumSteps(String s) {
         long ans = 0;
-        StringBuffer str = new StringBuffer(s);
-        int i = 0, j = str.length()-1;
-        while(i <= j){
-            if(str.charAt(i) == '1' && str.charAt(j) == '0'){
-                char temp = str.charAt(i);
-                str.setCharAt(i, str.charAt(j));
-                str.setCharAt(j, temp);
-                ans += j-i;
-            }
-            else if(str.charAt(i) == '1' && str.charAt(j) != '0')
-                i--;
-            else if(str.charAt(i) != '1' && str.charAt(j) == '0')
-                j++;
+        // StringBuffer str = new StringBuffer(s);
+        // int i = 0, j = str.length()-1;
+        // while(i <= j){
+        //     if(str.charAt(i) == '1' && str.charAt(j) == '0'){
+        //         char temp = str.charAt(i);
+        //         str.setCharAt(i, str.charAt(j));
+        //         str.setCharAt(j, temp);
+        //         ans += j-i;
+        //     }
+        //     else if(str.charAt(i) == '1' && str.charAt(j) != '0')
+        //         i--;
+        //     else if(str.charAt(i) != '1' && str.charAt(j) == '0')
+        //         j++;
 
             
-            i++;
-            j--;
+        //     i++;
+        //     j--;
+        // }
+        int j = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '0'){
+                ans += i - j;
+                j++;
+            }
         }
         return ans;
     }
