69a3cfa852466d4a1cf5026067bb1e9160c4b400
Shailendra0801 <110927376+Shailendra0801@users.noreply.github.com>
2024-10-19 21:17:01 +0530

    Time: 0 ms (100%), Space: 40.8 MB (73.93%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.java
@@ -1,23 +1,49 @@
 class Solution {
-    public char findKthBit(int n, int k) {
-        if(k == 0)
-            return '0';
-        StringBuilder s = new StringBuilder();
-        s.append('0');
-        for(int i = 1; i < n; i++){
-            StringBuilder str = new StringBuilder();
-            for(int j = 0; j < s.length(); j++){
-                if(s.charAt(j) == '1')
-                    str.append('0');
-                else
-                    str.append('1');
-            }
-            s.append('1');
-            s.append(str.reverse());
-            if(s.length() >= k)
-                return s.charAt(k-1);
-        }
+    // public char findKthBit(int n, int k) {
+    //     if(k == 0)
+    //         return '0';
+    //     StringBuilder s = new StringBuilder();
+    //     s.append('0');
+    //     for(int i = 1; i < n; i++){
+    //         StringBuilder str = new StringBuilder();
+    //         for(int j = 0; j < s.length(); j++){
+    //             if(s.charAt(j) == '1')
+    //                 str.append('0');
+    //             else
+    //                 str.append('1');
+    //         }
+    //         s.append('1');
+    //         s.append(str.reverse());
+    //         if(s.length() >= k)
+    //             return s.charAt(k-1);
+    //     }
 
-        return '0';
+    //     return '0';
+    // }
+
+    public char findKthBit(int n, int k) {
+        int cc=solve(n,k);
+        return cc==0?'0':'1';
+    }
+    public static int solve(int n,int k)
+    {
+        if(n==1||k==1)
+        {
+            return 0;
+        }
+        int len=(int)Math.pow(2,n)-1;
+        int mid=((len)/2);
+        if(mid==k-1)
+        {
+            return 1;
+        }
+        if(k-1<mid)
+        {
+            return solve(n-1,k);
+        }
+        else
+        {
+            return solve(n-1,len-k+1)==1?0:1;
+        }
     }
 }
