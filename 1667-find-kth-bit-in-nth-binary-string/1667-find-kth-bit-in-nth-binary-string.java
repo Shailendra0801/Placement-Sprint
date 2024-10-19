62dbc92a36df0df893fdb384b4eefc1fb7110031
Shailendra0801 <110927376+Shailendra0801@users.noreply.github.com>
2024-10-19 21:27:03 +0530

    Time: 0 ms (100%), Space: 41.1 MB (60.31%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.java
@@ -1,4 +1,5 @@
 class Solution {
+    // Approach 1
     // public char findKthBit(int n, int k) {
     //     if(k == 0)
     //         return '0';
@@ -21,29 +22,45 @@ class Solution {
     //     return '0';
     // }
 
-    public char findKthBit(int n, int k) {
-        int cc=solve(n,k);
-        return cc==0?'0':'1';
-    }
-    public static int solve(int n,int k)
-    {
-        if(n==1||k==1)
-        {
-            return 0;
-        }
-        int len=(int)Math.pow(2,n)-1;
-        int mid=((len)/2);
-        if(mid==k-1)
-        {
-            return 1;
-        }
-        if(k-1<mid)
-        {
-            return solve(n-1,k);
-        }
-        else
-        {
-            return solve(n-1,len-k+1)==1?0:1;
-        }
+    // Approach 2
+    // public char findKthBit(int n, int k) {
+    //     int cc=solve(n,k);
+    //     return cc==0?'0':'1';
+    // }
+    // public static int solve(int n,int k)
+    // {
+    //     if(n==1||k==1)
+    //     {
+    //         return 0;
+    //     }
+    //     int len=(int)Math.pow(2,n)-1;
+    //     int mid=((len)/2);
+    //     if(mid==k-1)
+    //     {
+    //         return 1;
+    //     }
+    //     if(k-1<mid)
+    //     {
+    //         return solve(n-1,k);
+    //     }
+    //     else
+    //     {
+    //         return solve(n-1,len-k+1)==1?0:1;
+    //     }
+    // }
+
+    // Approach 3
+    public char findKthBit(int n, int k){
+        if(k ==1)
+            return '0';
+        int l = (1 << n) - 1;
+        int mid = l/2 + 1;
+        if(k == mid)
+            return '1';
+        if(k < mid)
+            return findKthBit(n-1, k);
+        char invertedBit = findKthBit(n-1, l - k + 1);
+
+        return invertedBit == '0' ? '1' : '0';
     }
 }
