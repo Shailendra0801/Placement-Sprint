81e35414636be7250cfeee0175f106406d36f3c5
Shailendra0801 <110927376+Shailendra0801@users.noreply.github.com>
2024-10-21 15:31:28 +0530

    Time: 3 ms (100%), Space: 44.7 MB (84.5%) - LeetHub

--- 2825-minimize-string-length/2825-minimize-string-length.java
+++ 2825-minimize-string-length/2825-minimize-string-length.java
@@ -1,9 +1,28 @@
 class Solution {
     public int minimizedStringLength(String s) {
-        Set<Character> set = new HashSet<>();
-        for(char ch : s.toCharArray()){
-            set.add(ch);
+        // Approach 1
+        // Set<Character> set = new HashSet<>();
+        // for(char ch : s.toCharArray()){
+        //     set.add(ch);
+        // }
+        // return set.size();
+
+        // Approach 2
+        char[] chars=s.toCharArray();
+        int[] counts=new int[26];
+        int result=0;
+        for(char c:chars)
+        {
+            counts[c-'a']++;
         }
-        return set.size();
+        for(int i:counts)
+        {
+            if(i>0)
+            {
+                result++;
+            }
+        }
+        return result;
+    
     }
 }
