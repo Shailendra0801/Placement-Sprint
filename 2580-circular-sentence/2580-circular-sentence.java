9e9e074adc1f054e65f676ab0596adb49c8017dc
Shailendra0801 <110927376+Shailendra0801@users.noreply.github.com>
2024-11-02 15:29:26 +0530

    Time: 0 ms (100%), Space: 42 MB (37.29%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.java
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -1,14 +1,30 @@
 class Solution {
-    public boolean isCircularSentence(String sentence) {
-        for(int i = 0; i < sentence.length(); i++){
-            if(sentence.charAt(i) == ' '){
-                if(sentence.charAt(i-1) != sentence.charAt(i+1))
-                    return false;
-            }
-        }
-        if(sentence.charAt(0) != sentence.charAt(sentence.length()-1))
+    public boolean isCircularSentence(String s) {
+        // if(s.charAt(0) != s.charAt(s.length()-1))
+        //     return false;
+        // for(int i = 0; i < s.length(); i++){
+        //     if(s.charAt(i) == ' '){
+        //         if(s.charAt(i-1) != s.charAt(i+1))
+        //             return false;
+        //     }
+        // }
+
+        // return true;
+
+        if (s.charAt(0) != s.charAt(s.length() - 1))
             return false;
-            
+
+        int k = s.indexOf(" ");
+        if (k == -1)
+            return true;
+
+        while (k != -1) {
+            if (s.charAt(k - 1) != s.charAt(k + 1)) {
+                return false;
+            }
+
+            k = s.indexOf(" ", k+1);
+        }
         return true;
     }
 }
