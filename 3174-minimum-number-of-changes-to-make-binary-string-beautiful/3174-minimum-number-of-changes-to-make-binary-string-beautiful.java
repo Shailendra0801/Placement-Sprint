aa31aca2e58a3bf5ae786dd1251cc010c8cd1297
Shailendra0801 <110927376+Shailendra0801@users.noreply.github.com>
2024-11-05 10:52:29 +0530

    Time: 2 ms (100%), Space: 45.3 MB (9.38%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.java
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -1,15 +1,29 @@
 class Solution {
-    public int minChanges(String s) {
-        int ans = 0;
-        for(int i = 0; i < s.length(); i += 2){
-            boolean isTrue = check(s.substring(i, i+2));
-            if(!isTrue)
-                ans++;
+    // Approach 1
+    // public int minChanges(String s) {
+    //     int ans = 0;
+    //     for(int i = 0; i < s.length(); i += 2){
+    //         boolean isTrue = check(s.substring(i, i+2));
+    //         if(!isTrue)
+    //             ans++;
+    //     }
+    //     return ans;
+    // }
+
+    // private boolean check(String s){
+    //     return (s.equals("11") || s.equals("00"));
+    // }
+
+    // Approach 2
+    public int minChanges(String s){
+        int ans = 0,c = 0;
+        for(int i = 0; i < s.length(); i++){
+            c += s.charAt(i)-'0';
+            if(i%2 == 1){
+                ans += (c%2);
+                c = 0;
+            }
         }
         return ans;
-    }
-
-    private boolean check(String s){
-        return (s.equals("11") || s.equals("00"));
     }
 }
